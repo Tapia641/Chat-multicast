52f92b2cb7b62bac5724bb93399b74bb510f7d47
tapia <tapia641@gmail.com>
2019-03-07 23:43:11 -0600

    Create package

---
+++ src/servidor/servidor.java
@@ -0,0 +1,23 @@
+package servidor;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class servidor extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("servidorGUI.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
